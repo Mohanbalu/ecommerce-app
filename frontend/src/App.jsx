ea1bb17bb97663e6d62f0f243c18a76b20c627d9
Mohan Balu <mohanbalu292@gmail.com>
2025-10-15 15:52:38 +0530

    components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
-
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element = {<Home/>} />
+        <Route path='/collection' element = {<Collection/>} />
+        <Route path='/about' element = {<About/>} />
+        <Route path='/contact' element = {<Contact/>} />
+        <Route path='/product/:productId' element = {<Product/>} />
+        <Route path='/cart' element = {<Cart/>} />
+        <Route path='/place-order' element = {<PlaceOrder/>} />
+        <Route path='/orders' element = {<Orders/>} />
+      </Routes>
+    </div>
   )
 }
 
